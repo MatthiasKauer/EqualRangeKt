850da6d9b1824194743b0661e3eb14eaee075c3f
Matthias Kauer <mk.software@zuez.org>
2017-11-02 23:33:20 +0800

    simple walk to lower bound works

--- src/main/kotlin/equalRange.kt
+++ src/main/kotlin/equalRange.kt
@@ -1,6 +1,9 @@
 
 fun lowerBound(xs: List<Int>, x0: Int): Int {
     val i0 = xs.binarySearch(x0)
-    val iInsert = if (i0 < 0) (i0 + 1) * (-1) else i0
-    return iInsert
+    var iRet = if (i0 < 0) (i0 + 1) * (-1) else i0
+
+    while(iRet > 0 && xs[iRet - 1].compareTo(x0) == 0) iRet -= 1
+
+    return iRet
 }
--- src/test/kotlin/equalRangeTest.kt
+++ src/test/kotlin/equalRangeTest.kt
@@ -1,10 +1,16 @@
 
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
 
 class EqualRangeTest {
 
-    private val xs = listOf(10,20,30,30,50,50,60,80,30).sorted()
+    private val xs = listOf(10, 10, 10, 20, 30, 30, 30, 50, 50, 60, 80, 80)
+
+    @Before
+    fun setUp() {
+        assertEquals(xs.sorted(), xs)
+    }
 
     @Test
     fun simple() {
@@ -14,7 +20,7 @@ class EqualRangeTest {
     @Test
     fun lowerBoundForMissingInMiddle() {
         val i0 = lowerBound(xs, 25)
-        assertEquals(2, i0)
+        assertEquals(4, i0)
     }
 
     @Test
@@ -27,6 +33,24 @@ class EqualRangeTest {
     fun lowerBoundForMissingAtTail() {
         val i0 = lowerBound(xs, 95)
         assertEquals(xs.size, i0)
+    }
+
+    @Test
+    fun lowerBoundForExistingInMiddle() {
+        val i0 = lowerBound(xs, 30)
+        assertEquals(4, i0)
+    }
+
+    @Test
+    fun lowerBoundForExistingAtTail() {
+        val i0 = lowerBound(xs, 80)
+        assertEquals(xs.size - 2, i0)
+    }
+
+    @Test
+    fun lowerBoundForExistingAtHead() {
+        val i0 = lowerBound(xs, 10)
+        assertEquals(0, i0)
     }
 
 }
