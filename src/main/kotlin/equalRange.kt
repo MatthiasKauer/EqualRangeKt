84939d28c68651fac3fc4b08834cbbc086605df0
Matthias Kauer <mk.software@zuez.org>
2017-11-17 03:53:56 +0800

    finish 'golfed' functional equalRange implementation

--- src/main/kotlin/equalRange.kt
+++ src/main/kotlin/equalRange.kt
@@ -1,6 +1,6 @@
 fun <T: Comparable<T>> lowerBound(xs: List<T>, x0: T) : Int {
     if (xs is RandomAccess) {
-        return lowerBound1(xs, x0)
+        return lowerBoundSTL(xs, x0)
     } else {
         throw NotImplementedError("sorry")
     }
@@ -8,7 +8,7 @@ fun <T: Comparable<T>> lowerBound(xs: List<T>, x0: T) : Int {
 
 fun <T: Comparable<T>> upperBound(xs: List<T>, x0: T) : Int {
     if (xs is RandomAccess) {
-        return upperBound1(xs, x0)
+        return upperBoundSTL(xs, x0)
     } else {
         throw NotImplementedError("sorry")
     }
@@ -16,15 +16,39 @@ fun <T: Comparable<T>> upperBound(xs: List<T>, x0: T) : Int {
 
 fun <T: Comparable<T>> equalRange(xs: List<T>, x0: T) : Pair<Int, Int> {
     if (xs is RandomAccess) {
-        return equalRange0(xs, x0)
+        return equalRangeSTL(xs, x0)
     } else {
         throw NotImplementedError("sorry")
     }
 }
 
 
+data class State(var len: Int, var first: Int, var last: Int) {
+    fun m(): Int { return first + l2()}
+    fun l2(): Int { return len / 2}
+}
+fun <T: Comparable<T>> bound0(xs: List<T>, func : (State) -> Unit) : Int {
+    var state = State(len = xs.size, first = 0, last = xs.size);
+    while (state.len != 0) { func(state) }
+    return state.first
+}
+fun <T: Comparable<T>> bound1(xs: List<T>, pred: (T) -> Boolean): Int {
+    val func : (State) -> Unit = { s: State ->
+        if (pred(xs[s.m()])) {
+            s.first = s.m() + 1; s.len -= s.l2() + 1
+        } else {
+            s.len = s.l2()
+        }
+    }
+    return bound0(xs, func)
+}
+fun <T: Comparable<T>> lowerBound1(xs: List<T>, x0: T) = bound1(xs, {t: T -> t < x0 })
+fun <T: Comparable<T>> upperBound1(xs: List<T>, x0: T) = bound1(xs, {t: T -> t <= x0 })
+fun <T: Comparable<T>> equalRange1(xs: List<T>, x0: T) = Pair(lowerBound1(xs, x0), upperBound1(xs, x0))
+
+
 // modeled after C++ STL
-fun <T: Comparable<T>> lowerBound0(xs: List<T>, x0: T): Int {
+fun <T: Comparable<T>> lowerBoundSTL(xs: List<T>, x0: T): Int {
     var len = xs.size
     var first = 0
 
@@ -45,7 +69,7 @@ fun <T: Comparable<T>> lowerBound0(xs: List<T>, x0: T): Int {
 
 
 // modeled after C++ STL
-fun <T: Comparable<T>> upperBound0(xs: List<T>, x0: T) : Int {
+fun <T: Comparable<T>> upperBoundSTL(xs: List<T>, x0: T) : Int {
     var len = xs.size
     var first = 0
 
@@ -66,7 +90,7 @@ fun <T: Comparable<T>> upperBound0(xs: List<T>, x0: T) : Int {
 
 
 // modeled after C++ STL
-fun <T: Comparable<T>> equalRange0(xs: List<T>, x0: T) : Pair<Int, Int> {
+fun <T: Comparable<T>> equalRangeSTL(xs: List<T>, x0: T) : Pair<Int, Int> {
     var len = xs.size
     var first = 0
     var last = xs.size
@@ -83,47 +107,9 @@ fun <T: Comparable<T>> equalRange0(xs: List<T>, x0: T) : Pair<Int, Int> {
             len = l2
         } else {
             val mpl = m + 1
-            return Pair(first + lowerBound0(xs.subList(fromIndex = first, toIndex = m), x0),
-                        mpl + upperBound(xs.subList(fromIndex = mpl, toIndex = last),x0))
+            return Pair(first + lowerBoundSTL(xs.subList(fromIndex = first, toIndex = m), x0),
+                    mpl + upperBoundSTL(xs.subList(fromIndex = mpl, toIndex = last),x0))
         }
     }
     return Pair(first, first)
-}
-
-
-data class State(var len: Int, var first: Int, var last: Int) {
-    fun m(): Int { return first + l2()}
-    fun l2(): Int { return len / 2}
-}
-fun <T: Comparable<T>> bound0(xs: List<T>, x0: T, func : (State) -> Unit) : Int {
-    var state = State(len = xs.size, first = 0, last = xs.size);
-
-    while (state.len != 0) {
-        func(state)
-    }
-    return state.first
-}
-fun <T: Comparable<T>> bound1(xs: List<T>, x0: T, pred: (T) -> Boolean): Int {
-    val func : (State) -> Unit = { s: State ->
-        if (pred(xs[s.m()])) {
-            s.first = s.m() + 1; s.len -= s.l2() + 1
-        } else {
-            s.len = s.l2()
-        }
-    }
-    return bound0(xs, x0, func)
-}
-
-fun <T: Comparable<T>> lowerBound1(xs: List<T>, x0: T): Int {
-    return bound1(xs, x0, {t: T -> t < x0 })
-}
-fun <T: Comparable<T>> upperBound1(xs: List<T>, x0: T): Int {
-    val func : (State) -> Unit = { s: State ->
-        if (xs[s.m()] > x0) {
-            s.len = s.l2()
-        } else {
-            s.first = s.m() + 1; s.len -= s.l2() + 1
-        }
-    }
-    return bound0(xs, x0, func)
 }
