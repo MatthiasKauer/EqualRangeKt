44bab4000d04c963585df2c64e252c1b1c9ebff5
Matthias Kauer <mk.software@zuez.org>
2017-11-17 09:15:01 +0800

    add simple Java test

--- src/test/java/EqualRangeJavaTest.java
+++ src/test/java/EqualRangeJavaTest.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
 
-public class EqualRangeTest {
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class EqualRangeJavaTest {
+    // only tests that the library can be called from Java, not corner cases
+
+    @Before
+    public void setUp() {
+        xs = new ArrayList<>();
+        xs.add(10);
+        xs.add(30);
+        xs.add(30);
+        xs.add(40);
+    }
+
+    @Test
+    public void existingInMiddle() {
+        assertEquals(1, EqualRangeSTLKt.lowerBound(xs, 30));
+        assertEquals(3, EqualRangeSTLKt.upperBound(xs, 30));
+    }
+
+    private List<Integer> xs;
 }
